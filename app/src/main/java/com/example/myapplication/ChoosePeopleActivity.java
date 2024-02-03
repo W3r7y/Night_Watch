6ab2ba30f15250f30bcf0c0bae7fea106d2f3fe9
Ernest Vaulin <shenoby258@gmail.com>
2024-02-03 13:20:39 +0100

    Fixing time picker's setup and visualization on TextView

--- app/src/main/java/com/example/myapplication/ChoosePeopleActivity.java
+++ app/src/main/java/com/example/myapplication/ChoosePeopleActivity.java
@@ -6,17 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class DisplayMessageActivity extends AppCompatActivity {
+public class ChoosePeopleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message_activities);
+        setContentView(R.layout.choose_people_activity);
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.MESSAGE_KEY);
 
-        TextView textView = (TextView) findViewById(R.id.message_display_text_view);
+        TextView textView = (TextView) findViewById(R.id.choose_people_tv);
         textView.setText(message);
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,12 +6,8 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.util.Calendar;
 
@@ -30,43 +26,60 @@ public class MainActivity extends AppCompatActivity {
         //EditText editText = (EditText) findViewById(R.id.edit_text);
         //String message = editText.getText().toString();
 
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Intent intent = new Intent(this, ChoosePeopleActivity.class);
         // intent.putExtra(MESSAGE_KEY, message);
         startActivity(intent);
     }
 
-    public void clickButtonTimePick(View view){
-
+    public void clickTimePicker(View view) {
         Calendar calendar = Calendar.getInstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
         int min = calendar.get(Calendar.MINUTE);
 
+        int buttonId = view.getId();
+        int textViewId;
+
+        if (buttonId == R.id.pickTime1) {
+            textViewId = R.id.tvForTime1;
+        } else if (buttonId == R.id.pickTime2) {
+            textViewId = R.id.tvForTime2;
+        } else {
+            // Handle unexpected button click
+            return;
+        }
+
         TimePickerDialog timePicker;
         timePicker = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
             @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                // Set time to text view
-                TextView tv = (TextView) findViewById(R.id.tvForTime1);
-
-                String hours = "";
-                String minutes = "";
-                if(hourOfDay < 10){
-                    hours = "0" + hourOfDay;
-                }else{
-                    hours = "" + hourOfDay;
-                }
-                if(minute < 10){
-                    minutes = "0" + minute;
-                }else{
-                    minutes = "" + minute;
-                }
-
-                tv.setText(hours + ":" + minutes);
+            public void onTimeSet(TimePicker view, int hour, int minute) {
+                // Set time to the selected text view
+                TextView tv = findViewById(textViewId);
+                tv.setText(timeToString(hour, minute));
             }
         }, hour, min, true);
 
         timePicker.setTitle("Select Time");
         timePicker.show();
+    }
+
+    public String timeToString(int hours, int minutes){
+        String time = "";
+        String hourStr = "";
+        String minuteStr = "";
+
+        if(hours < 10){
+            hourStr = "0" + hours;
+        }else{
+            hourStr = "" + hours;
+        }
+        if(minutes < 10){
+            minuteStr = "0" + minutes;
+        }else{
+            minuteStr = "" + minutes;
+        }
+
+        time = hourStr + ":" + minuteStr;
+        return time;
     }
 
     //Check

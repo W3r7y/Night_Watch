7cc52f538fb0eb393089fe2930cdf9939923a3c9
E.V <111234974+W3r7y@users.noreply.github.com>
2024-02-16 15:58:39 +0100

    Adding names to ListView feature

--- app/src/main/java/com/example/myapplication/ChoosePeopleActivity.java
+++ app/src/main/java/com/example/myapplication/ChoosePeopleActivity.java
@@ -7,14 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class ChoosePeopleActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> names;
+    ArrayAdapter<String> adapter;
+    EditText input;
+    ImageView add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +39,33 @@ public class ChoosePeopleActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.time_difference_tv);
         textView.setText("Total time: " + calculateDifferenceBetweenTime(time1, time2));
 
+        listView = findViewById(R.id.list_view);
+        input = findViewById(R.id.input);
+        add = findViewById(R.id.add);
+
+        names = new ArrayList<>();
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, names);
+        listView.setAdapter(adapter);
+
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = input.getText().toString();
+                if(text == null || text.length() == 0){
+                    makeToast("Enter a Name");
+                }
+                else{
+                    addName(text);
+                    input.setText("");
+                    makeToast("Added: " + text);
+                }
+            }
+        });
+
+
+        /*
         //TODO: custom style for spinner
         Spinner spinner =(Spinner) findViewById(R.id.spinner);
         Integer[] items = new Integer[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,100};
@@ -47,8 +85,21 @@ public class ChoosePeopleActivity extends AppCompatActivity {
                 // make a joke
             }
         });
+         */
     }
 
+    public void addName(String name){
+        names.add(name);
+        listView.setAdapter(adapter);
+    }
+    Toast t;
+    private void makeToast(String s){
+        if (t != null) {
+            t.cancel();
+        }
+        t = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT);
+        t.show();
+    }
 
     public String calculateDifferenceBetweenTime(String t1, String t2){
         SimpleDateFormat format = new SimpleDateFormat("HH:mm");
